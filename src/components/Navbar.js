488f6d563478925894e09db0af78f7ef66f802b5
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-03 16:49:31 -0400

    initial upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+ 
+function Navbar() {
+    return (
+        <header>
+            <div>
+                <a href='#'>TV For Me</a>
+            </div>
+            <nav>
+                <div>
+                    <input type='text' required></input>
+                </div>
+                <a href='#'>Action</a>
+                <a href='#'>Comedy</a>
+                <a href='#'>Crime</a>
+                <a href='#'> Drama</a>
+                <a href='#'>Horror</a>
+                <a href='#'>Romance</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
