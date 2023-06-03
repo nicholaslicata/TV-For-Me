cbef652beb8aee91b49dded2c3fb494af0abbb5a
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-03 16:49:20 -0400

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { useState, useEffect } from 'react';
+
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
+
+  useEffect(() => {
+    getTVData();
+  })
+
+  const comedies = [];
+
+  async function getTVData() {
+    const randomNumber = Math.random() * 50;
+    const wholeNumber = Math.floor(randomNumber);
+    const api = `https://api.tvmaze.com/shows`
+    // console.log(randomNumber);
+    // console.log(wholeNumber);
+
+    
+     try {
+      const response = await fetch(api);
+      const data = await response.json();
+      // console.log(data);
+      // data.forEach((d) => {
+      //   if (d.genres.includes('Romance')) {
+      //     comedies.push(d);
+      //     console.log(d.name);
+      //   }
+      // })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div>
-     
+    <div> 
+      <Navbar />
     </div>
   );
 }
