6e4db94d7d32c8203dbc345d7f0497b7bc10659d
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-04 23:48:51 -0400

    updated nav layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
  import { Link } from 'react-router-dom';
+ import { FaSearch } from 'react-icons/fa'
 
-function Navbar({ toggleNav, navActive, closeNav }) {
+function Navbar({ toggleNav, navActive, closeNav, toggleInput, inputActive, closeInput  }) {
     return (
         <header className='navbar-container'>
+            <div onClick={toggleNav} className='hamburger-btn'>
+                <span className='hamburger-line'></span>
+                <span className='hamburger-line'></span>
+                <span className='hamburger-line'></span>
+            </div>
             <div className='logo-container'>
                 <Link onClick={closeNav} className='logo' to='/'>TV For Me</Link>
             </div>
+            <button onClick={toggleInput} className='search-btn'>
+                <FaSearch />
+            </button>
+            <div className={inputActive ? 'input-active' : 'input-container'}>
+                <input className='nav-input' type='text' placeholder='Search for shows' required></input>
+            </div>
             <nav className={navActive ? 'nav-active' : 'nav'}>
-                <div className='input-container'>
-                    <input className='nav-input' type='text' placeholder='Search for shows' required></input>
-                </div>
                 <Link onClick={closeNav} className='nav-link' to='action'>Action</Link>
                 <Link onClick={closeNav} className='nav-link' to='comedy'>Comedy</Link>
                 <Link onClick={closeNav} className='nav-link' to='crime'>Crime</Link>
@@ -18,11 +27,6 @@ function Navbar({ toggleNav, navActive, closeNav }) {
                 <Link onClick={closeNav} className='nav-link' to='romance'>Romance</Link>
                 <Link onClick={closeNav} className='nav-link' to='mywatchlist'>My Watchlist</Link>
             </nav>
-            <div onClick={toggleNav} className='hamburger-btn'>
-                    <span className='hamburger-line'></span>
-                    <span className='hamburger-line'></span>
-                    <span className='hamburger-line'></span>
-                </div>
         </header>
     )
 }
