25e3e76696f0e4f568af6cbe336d49d78f16986b
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-11 21:33:29 -0400

    setup person page layout

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 
-function Person() {
+function Person({ personDetails }) {
+
+    // const personApi = `https://api.tvmaze.com/people/${personDetails.id}/castcredits?embed=show`;
+
+    // useEffect(() => {
+    //     fetch(personApi)
+    //       .then(res => {
+    //        return res.json();
+    //     })
+    //       .then(data => {
+    //         console.log(data);
+    //     })
+    // }, [])  
+
+    // console.log(personDetails.id);
+
     return (
-        <h1>Person</h1>
+        <div>
+            <h1>{personDetails.name}</h1>
+            <img src={!personDetails.img ? require('../assets/noImage.png') : personDetails.img.medium} className="show-img" alt={personDetails.name}></img>
+        </div>
     )
 }
 
