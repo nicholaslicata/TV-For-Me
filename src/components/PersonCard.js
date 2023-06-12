1c8d295f5d6e7725c9f2b810508131d58b4ca7f4
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-11 21:31:28 -0400

    added id to personDetails

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
 
-function PersonCard() {
+// Pages
+import Person from '../pages/Person';
+
+function PersonCard({ personImage, personDetails, setPersonDetails, people}) {
+    
+    function updatePersonDetails() {
+        setPersonDetails({
+            name: people.name,
+            img: personImage,
+            id: people.id,
+        })
+    }
+
     return (
-        <h1>Person</h1>
+        <div onClick={updatePersonDetails} className='person-card'>
+            <Link to='/person:id'>
+                <img src={personImage ? personImage.medium : require('../assets/noImage.png')} className='person-card-img'></img>
+            </Link>
+            <p className='person-card-name'>{people.name}</p>
+        </div>
     )
 }
 
