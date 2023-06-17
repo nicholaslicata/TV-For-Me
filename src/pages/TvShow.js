d36148c901c215051803486b4674c0c326be350c
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-16 21:46:21 -0400

    added error handling for null data

--- src/pages/TvShow.js
+++ src/pages/TvShow.js
@@ -9,28 +9,28 @@ function TvShow({ showDetails }) {
                 <img src={!showDetails.img ? require('../assets/noImage.png') : showDetails.img.medium} className="show-page-img" alt={showDetails.name}></img>
               </div>
               <div className='show-page-inner-container'>
-                <h1 className='show-page-title'>{showDetails.name}</h1>
-                <p className='show-page-rating'>Rating: {showDetails.rating}</p>
+                <h1 className='show-page-title'>{showDetails.name === null ? 'N/A' : showDetails.name}</h1>
+                <p className='show-page-rating'>Rating: {showDetails.rating === null ? 'N/A' : showDetails.rating}</p>
                 <AddBtn />
-                <p className='show-page-summary'>{showDetails.summary.replace(/(<([^>]+)>)/gi, '')}</p>
+                <p className='show-page-summary'>{showDetails.summary === null ? 'N/A' : showDetails.summary.replace(/(<([^>]+)>)/gi, '')}</p>
               </div>
             </section>
             <aside className='show-page-secondary-content'>
                 <div className='show-page-divider'>
                     <p className='show-page-divider-title'>Genres:</p>
-                    <p className='show-page-divider-subtext'>{showDetails.genres}</p>
+                    <p className='show-page-divider-subtext'>{`${showDetails.genres === null ? 'N/A' : showDetails.genres}`}</p>
                 </div>
                 <div className='show-page-divider'>
                     <p className='show-page-divider-title'>Premiere date:</p>
-                    <p className='show-page-divider-subtext'>{showDetails.premiered}</p>
+                    <p className='show-page-divider-subtext'>{showDetails.premiered === null ? 'N/A' : showDetails.premiered}</p>
                 </div>
                 <div className='show-page-divider'>
                     <p className='show-page-divider-title'>End date:</p>
-                    <p className='show-page-divider-subtext'>{showDetails.ended}</p>
+                    <p className='show-page-divider-subtext'>{showDetails.ended === null ? 'N/A' : showDetails.ended}</p>
                 </div>
                 <div className='show-page-divider'>
                     <p className='show-page-divider-title'>Network:</p>
-                    <p className='show-page-divider-subtext'>{showDetails.network}</p>
+                    <p className='show-page-divider-subtext'>{showDetails.network === null ? 'N/A' : showDetails.network}</p>
                 </div>
             </aside>
         </main>
