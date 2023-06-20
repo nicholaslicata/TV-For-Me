ab3eccfe353de66ab545bbeb55842b4e951e1561
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-19 20:31:55 -0400

    setup page layout

--- src/pages/Horror.js
+++ src/pages/Horror.js
@@ -1,7 +1,23 @@
+import ShowCard from '../components/ShowCard';
 
-function Horror() {
+function Horror({ showsData, showDetails, setShowDetails }) {
+
+    const horrorShows = showsData.filter((shows) => { 
+        if (shows.network && shows.genres.includes('Horror')) {
+            return shows;
+        }
+    });
+
     return (
-        <h1>Horror</h1>
+        <main className='genre-page-container'>
+          <div className='genre-shows-container'>
+          {horrorShows.map(shows => {
+              return (
+                   <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+              )
+          })}
+          </div>
+       </main>
     )
 }
 
