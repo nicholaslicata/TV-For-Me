9e38511e1062d86258acfbcf3aede7874b0fb49a
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-19 20:31:28 -0400

    setup page layout

--- src/pages/Crime.js
+++ src/pages/Crime.js
@@ -1,7 +1,23 @@
+import ShowCard from '../components/ShowCard';
 
-function Crime() {
+function Crime({ showsData, showDetails, setShowDetails }) {
+
+    const crimeShows = showsData.filter((shows) => { 
+        if (shows.network && shows.genres.includes('Crime')) {
+            return shows;
+        }
+    });
+
     return (
-        <h1>Crime</h1>
+       <main className='genre-page-container'>
+          <div className='genre-shows-container'>
+          {crimeShows.map(shows => {
+                return (
+                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                )
+          })}
+          </div>
+       </main>
     )
 }
 
