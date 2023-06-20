86ca5c671e0cceeaeaa023b93982ba2f82889d98
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-19 20:30:39 -0400

    updated

--- src/pages/Action.js
+++ src/pages/Action.js
@@ -1,7 +1,23 @@
+import ShowCard from '../components/ShowCard';
 
-function Action() {
+function Action({ showsData, showDetails, setShowDetails }) {
+
+     const actionShows = showsData.filter((shows) => {
+         if (shows.network && shows.genres.includes('Action')) {
+             return shows;
+        }
+    });
+
     return (
-        <h1>Action</h1>
+        <main className='genre-page-container'>
+          <div className='genre-shows-container'>
+          {actionShows.map(shows => {
+                 return (
+                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                 )
+          })}
+          </div>
+        </main>
     )
 }
 
