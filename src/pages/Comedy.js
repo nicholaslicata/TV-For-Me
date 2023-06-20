8ba8ed781b8dec5bd7209367ef08f9c7f474b6e7
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-19 20:31:00 -0400

    setup page layout

--- src/pages/Comedy.js
+++ src/pages/Comedy.js
@@ -1,7 +1,23 @@
+import ShowCard from '../components/ShowCard';
 
-function Comedy() {
+function Comedy({ showsData, showDetails, setShowDetails }) {
+
+    const comedyShows = showsData.filter((shows) => { 
+        if (shows.network && shows.genres.includes('Comedy')) {
+            return shows;
+        }
+    });
+
     return (
-        <h1>Comedy</h1>
+        <main className='genre-page-container'>
+            <div className='genre-shows-container'>
+            {comedyShows.map(shows => {
+                   return(
+                       <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                   )
+            })}
+            </div>
+        </main>
     )
 }
 
