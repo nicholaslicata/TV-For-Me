aaf868bd9542cc1aaa95cb48d712a82bfed72cd4
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-19 20:31:40 -0400

    setup page layout

--- src/pages/Drama.js
+++ src/pages/Drama.js
@@ -1,7 +1,23 @@
+import ShowCard from '../components/ShowCard';
 
-function Drama() {
+function Drama({ showsData, showDetails, setShowDetails }) {
+
+    const dramaShows = showsData.filter((shows) => { 
+        if (shows.network && shows.genres.includes('Drama')) {
+            return shows;
+        }
+    });
+
     return (
-        <h1>Drama</h1>
+        <main className='genre-page-container'>
+           <div className='genre-shows-container'>
+           {dramaShows.map(shows => {
+              return (
+                   <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+              )
+           })}
+          </div>
+       </main>
     )
 }
 
