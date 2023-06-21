cf8e6cbf4d0402ec6fac1a2474aa3773237a3e24
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-20 21:06:53 -0400

    updated

--- src/pages/MyWatchlist.js
+++ src/pages/MyWatchlist.js
@@ -1,7 +1,17 @@
+import ShowCard from '../components/ShowCard';
 
-function MyWatchlist() {
+function MyWatchlist({ showDetails, setShowDetails, watchList }) {
     return (
-        <h1>MyWatchlist</h1>
+       <main className='genre-page-container'>
+          {watchList.length > 0 ? (
+            <div className='genre-shows-container'>
+                {watchList.map((shows) => (
+                    <ShowCard shows={shows} showNetwork={shows.network} showRating={shows.rating} showImage={shows.img} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                ))}
+            </div>
+          ) : (<h1 className='watchlist-empty'>Your watch list is empty</h1>)
+          }
+       </main>
     )
 }
 
