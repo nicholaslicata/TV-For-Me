dfa39ae530195c57c630dc70234297159f260909
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-20 21:03:37 -0400

    updated

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -1,7 +1,7 @@
 
-function AddBtn() {
+function AddBtn({ handleAddShow }) {
     return (
-        <button className='add-btn'>Add To Watchlist</button>
+        <button className='add-btn' onClick={handleAddShow}>Add To Watchlist</button>
     )
 }
 
