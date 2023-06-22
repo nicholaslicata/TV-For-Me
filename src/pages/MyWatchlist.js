9bccda054fb467faaa86d61947dfbfca91691dd0
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-22 00:39:04 -0400

    updated id prop passsed to DeleteBtn

--- src/pages/MyWatchlist.js
+++ src/pages/MyWatchlist.js
@@ -1,13 +1,17 @@
 import ShowCard from '../components/ShowCard';
+import DeleteBtn from '../components/DeleteBtn';
 
-function MyWatchlist({ showDetails, setShowDetails, watchList }) {
+function MyWatchlist({ showDetails, setShowDetails, watchList, handleDeleteShow }) {
     return (
        <main className='genre-page-container'>
           {watchList.length > 0 ? (
             <div className='genre-shows-container'>
                 {watchList.map((shows) => (
+                    <div>
                     <ShowCard shows={shows} showNetwork={shows.network} showRating={shows.rating} showImage={shows.img} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
-                ))}
+                    <DeleteBtn handleDeleteShow={handleDeleteShow} showId={shows.id} />
+                    </div>
+                ))} 
             </div>
           ) : (<h1 className='watchlist-empty'>Your watch list is empty</h1>)
           }
