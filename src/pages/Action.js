17e677856c0a88564d0a92ceba4f4b48e0dbd3eb
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-21 20:37:57 -0400

    fixed genre undefined bug

--- src/pages/Action.js
+++ src/pages/Action.js
@@ -13,7 +13,7 @@ function Action({ showsData, showDetails, setShowDetails }) {
           <div className='genre-shows-container'>
           {actionShows.map(shows => {
                  return (
-                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
                  )
           })}
           </div>
