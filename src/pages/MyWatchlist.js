27f8e922fb7616db19bd47a52286918ff379ee7b
Nicholas Licata <nicholas.licata730@gmail.com>
2023-07-01 13:35:43 -0400

    fixed error status bug

--- src/pages/MyWatchlist.js
+++ src/pages/MyWatchlist.js
@@ -1,14 +1,14 @@
 import ShowCard from '../components/ShowCard';
 import DeleteBtn from '../components/DeleteBtn';
 
-function MyWatchlist({ showDetails, setShowDetails, watchList, handleDeleteShow }) {
+function MyWatchlist({ showDetails, setShowDetails, watchList, handleDeleteShow, isError, setIsError }) {
     return (
        <main className='genre-page-container'>
           {watchList.length > 0 ? (
             <div className='genre-shows-container'>
                 {watchList.map((shows) => (
                     <div>
-                    <ShowCard shows={shows} showNetwork={shows.network} showRating={shows.rating} showImage={shows.img} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                    <ShowCard shows={shows} showNetwork={shows.network} showRating={shows.rating} showImage={shows.img} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} isError={isError} setIsError={setIsError} key={shows.id} />
                     <DeleteBtn handleDeleteShow={handleDeleteShow} showId={shows.id} />
                     </div>
                 ))} 
