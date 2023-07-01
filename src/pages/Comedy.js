ba22bb4ff8875411516b599a3669442465f2fb88
Nicholas Licata <nicholas.licata730@gmail.com>
2023-07-01 13:34:18 -0400

    fixed error status bug

--- src/pages/Comedy.js
+++ src/pages/Comedy.js
@@ -1,6 +1,6 @@
 import ShowCard from '../components/ShowCard';
 
-function Comedy({ showsData, showDetails, setShowDetails }) {
+function Comedy({ showsData, showDetails, setShowDetails, isError, setIsError }) {
 
     const comedyShows = showsData.filter((shows) => { 
         if (shows.network && shows.genres.includes('Comedy')) {
@@ -13,7 +13,7 @@ function Comedy({ showsData, showDetails, setShowDetails }) {
             <div className='genre-shows-container'>
             {comedyShows.map(shows => {
                    return(
-                       <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                       <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} isError={isError} setIsError={setIsError} key={shows.id} />
                    )
             })}
             </div>
