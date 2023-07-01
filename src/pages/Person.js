ceae4b540b285fef12a69371e8b521a2b84cdb9a
Nicholas Licata <nicholas.licata730@gmail.com>
2023-06-30 22:51:33 -0400

    setup person page layout

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react';
+import uniqid from 'uniqid';
 
-function Person({ personDetails, personShow, setPersonShow }) {
+function Person({ personDetails }) {
     const [personData, setPersonData] = useState();
 
     const personApi = `https://api.tvmaze.com/people/${personDetails.id}/castcredits?embed=show`;
-
-    // const personShows = personData.map((personShow => {
-    //     console.log(personShow);
-    // }))
 
        useEffect(() => {
         fetch(personApi)
@@ -16,24 +13,36 @@ function Person({ personDetails, personShow, setPersonShow }) {
         })
         .then(data => {
           setPersonData(data);
-          console.log(personData);
         })
     }, [])
 
-        // useEffect(() => {
-        //     console.log(personData)
-        // }, [personData])
-
-
-
     return (
-        <div>
-            <img src={!personDetails.img ? require('../assets/noImage.png') : personDetails.img.medium} className="show-img" alt={personDetails.name}></img>
-            <h1>{personDetails.name = null ? 'N/A' : personDetails.name}</h1>
-            <p>{personDetails.birthday === null ? 'N/A' : personDetails.birthday}</p>
-            <p>{personDetails.country === null ? 'N/A' : personDetails.country.name}</p>
-            {/* <p>Know for {personData[0]._links}</p> */}
-        </div>
+        <main className='person-page-container'>
+            <div className='person-page-img-container'>
+              <img src={!personDetails.img ? require('../assets/noImage.png') : personDetails.img.medium} className="person-page-img" alt={personDetails.name}></img>
+            </div>
+            <section className='person-page-text'>
+              <h1 className='person-page-name'>{personDetails.name = null ? 'N/A' : personDetails.name}</h1>
+                <div className='person-page-divider'>
+                  <p className='person-page-divider-title'>Birthday:</p>
+                  <p className='person-page-divider-subtext'>{personDetails.birthday === null ? 'N/A' : personDetails.birthday}</p>
+                </div>
+                <div className='person-page-divider'>
+                  <p className='person-page-divider-title'>Birth Place:</p>
+                  <p className='person-page-divider-subtext'>{personDetails.country === null ? 'N/A' : personDetails.country.name}</p>
+                </div>
+                <div className='person-page-divider'>
+                  <p className='person-page-divider-title'>Known for:</p>
+                  <ul>
+                  {personData ? personData.map(show => {
+                    return (
+                        <li key={uniqid()} className='known-for-shows'>{show._embedded.show.name === null ? 'N/A' : show._embedded.show.name}</li>
+                      )
+                   }) : null}
+                  </ul>
+                </div>
+            </section>
+        </main>
     )
 }
 
