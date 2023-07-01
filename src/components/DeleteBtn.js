343d3dd2d488712e987050445215b0ca73db9289
Nicholas Licata <nicholas.licata730@gmail.com>
2023-07-01 18:53:36 -0400

    added aria-label to button

--- src/components/DeleteBtn.js
+++ src/components/DeleteBtn.js
@@ -2,7 +2,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 function DeleteBtn({ handleDeleteShow, showId }) {
     return (
-        <button onClick={() => handleDeleteShow(showId)} className='delete-btn'>
+        <button onClick={() => handleDeleteShow(showId)} className='delete-btn' aria-label='delete-from-watchlist'>
             <AiOutlineCloseCircle className='delete-icon' />
         </button>
     )
