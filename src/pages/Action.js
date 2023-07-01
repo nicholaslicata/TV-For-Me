a2134c465dffba9c7189e700e9c4747ddd41cf5b
Nicholas Licata <nicholas.licata730@gmail.com>
2023-07-01 13:34:05 -0400

    fixed error status bug

--- src/pages/Action.js
+++ src/pages/Action.js
@@ -1,6 +1,6 @@
 import ShowCard from '../components/ShowCard';
 
-function Action({ showsData, showDetails, setShowDetails }) {
+function Action({ showsData, showDetails, setShowDetails, isError, setIsError }) {
 
      const actionShows = showsData.filter((shows) => {
          if (shows.network && shows.genres.includes('Action')) {
@@ -13,7 +13,7 @@ function Action({ showsData, showDetails, setShowDetails }) {
           <div className='genre-shows-container'>
           {actionShows.map(shows => {
                  return (
-                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} key={shows.id} />
+                     <ShowCard shows={shows} showNetwork={shows.network.name} showRating={shows.rating.average} showImage={shows.image} showGenre={shows.genres} showDetails={showDetails} setShowDetails={setShowDetails} isError={setIsError} setIsError={setIsError} key={shows.id} />
                  )
           })}
           </div>
